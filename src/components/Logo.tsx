c84a95df1ca6811756f1e95233cff7f1534ca3a7
changhak0310 <dlckdgkr0320@naver.com>
2023-06-16 03:13:46 +0900

    Plus-Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+const LogoStyle = styled.div`
+    font: small-caps bold 24px/1 sans-serif;
+`
+
+export const Logo = () => {
+    return(
+        <LogoStyle>
+            MyPage
+        </LogoStyle>
+    )
+}
--- src/modules/header.tsx
+++ src/modules/header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Stack } from '@/designSystem/Stack';
+import { Logo } from '@/components/logo';
 
 const Header: React.FC = () => {
   return (
     <Stack
-      wrap
+      distribute="space-between"
+      padding={[0,40,0,40]}
       style={{background: "#c3b5b5"}}
+      height={120}
     >
-      <div style={{background: "#a33535", padding:"30px"}}>Logo</div>
+      <Logo/>
       <div style={{background: "#35a356", padding:"30px"}}>Menu</div>
 
     </Stack>
