bd228bf8de10ba1494708b1a6939f3cf9733c744
changhak0310 <dlckdgkr0320@naver.com>
2023-06-19 23:29:46 +0900

    Plus-MainFont Style

---
+++ src/components/FontStyle/MainFont.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from '@emotion/styled'
+
+export const MainFont = styled.span`
+    line-height: 1.1;
+    font-weight: 700;
+    text-shadow: none;
+    font-size: 5.8vw;
+`
